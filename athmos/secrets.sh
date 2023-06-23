9c4222c84a7e69da9d9cd70d3fa6a11d4a239349
paulb <barrie.paul@protonmail.com>
2023-06-23 18:48:23 +0200

    feat: add main dependencies

---
+++ athmos/secrets.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+COMMON_NAMESPACE=common
+INFRA_NAMESPACE=infra
+GATEWAY_NAMESPACE=gateway
+
+kubectl create namespace "$COMMON_NAMESPACE"  > /dev/null 2>&1
+kubectl create namespace "$INFRA_NAMESPACE" > /dev/null 2>&1
+kubectl create namespace "$GATEWAY_NAMESPACE" > /dev/null 2>&1
+
+CLOUDFLARE_SECRET_NAME=cloudflare-secret
+CLOUDFLARE_SECRET_KEY=cloudflare-api-token
+kubectl -n $COMMON_NAMESPACE create secret generic $CLOUDFLARE_SECRET_NAME --from-file=$CLOUDFLARE_SECRET_KEY=$HOME/.athmos/cloudflare > /dev/null 2>&1
+
+# NEO4J_NAMESPACE=gateway
+# NEO4J_SECRET_NAME=neo4j-secrets
+# NEO4J_PASSWORD=$(openssl rand -base64 15)
+
+# kubectl create namespace "$NEO4J_NAMESPACE" > /dev/null 2>&1
+# kubectl -n $NEO4J_NAMESPACE create secret generic $NEO4J_SECRET_NAME --from-literal=NEO4J_DATA=$NEO4J_PASSWORD > /dev/null 2>&1
+
+MONGODB_SECRET_NAME=mongodb-auth
+MONGODB_PASSWORD=$(openssl rand -base64 15)
+MONGODB_ROOT_PASSWORD=$(openssl rand -base64 15)
+kubectl -n $INFRA_NAMESPACE create secret generic  $MONGODB_SECRET_NAME \
+            --from-literal=mongodb-passwords=$MONGODB_SECRET_NAME --from-literal=mongodb-root-password=$MONGODB_ROOT_PASSWORD> /dev/null 2>&1
+
+COMMON_NAMESPACE=common
+RABBITMQ_SECRET_NAME=rabbitmq-auth
+RABBITMQ_PASSWORD=$(openssl rand -base64 15)
+
+kubectl -n $COMMON_NAMESPACE create secret generic $RABBITMQ_SECRET_NAME \
+            --from-literal=rabbitmq-password=$RABBITMQ_SECRET_NAME > /dev/null 2>&1
+
+kubectl -n $INFRA_NAMESPACE create secret generic $RABBITMQ_SECRET_NAME \
+            --from-literal=rabbitmq-password=$RABBITMQ_SECRET_NAME > /dev/null 2>&1
+
+kubectl -n $GATEWAY_NAMESPACE create secret generic $RABBITMQ_SECRET_NAME \
+            --from-literal=rabbitmq-password=$RABBITMQ_SECRET_NAME > /dev/null 2>&1
