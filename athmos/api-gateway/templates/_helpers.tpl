9b82db7ba50fb7575396471903bf4cfedf9f4f9f
paulb <barrie.paul@protonmail.com>
2023-06-28 16:04:02 +0200

    feat: app and api charts

--- athmos/api-gateway/templates/_helpers.tpl
+++ athmos/api-gateway/templates/_helpers.tpl
@@ -1,7 +1,7 @@
 {{/*
 Expand the name of the chart.
 */}}
-{{- define "infra-worker.name" -}}
+{{- define "api-gateway.name" -}}
 {{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
@@ -10,7 +10,7 @@ Create a default fully qualified app name.
 We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
 If release name contains chart name it will be used as a full name.
 */}}
-{{- define "infra-worker.fullname" -}}
+{{- define "api-gateway.fullname" -}}
 {{- if .Values.fullnameOverride }}
 {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
 {{- else }}
@@ -26,16 +26,16 @@ If release name contains chart name it will be used as a full name.
 {{/*
 Create chart name and version as used by the chart label.
 */}}
-{{- define "infra-worker.chart" -}}
+{{- define "api-gateway.chart" -}}
 {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
 {{/*
 Common labels
 */}}
-{{- define "infra-worker.labels" -}}
-helm.sh/chart: {{ include "infra-worker.chart" . }}
-{{ include "infra-worker.selectorLabels" . }}
+{{- define "api-gateway.labels" -}}
+helm.sh/chart: {{ include "api-gateway.chart" . }}
+{{ include "api-gateway.selectorLabels" . }}
 {{- if .Chart.AppVersion }}
 app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
 {{- end }}
@@ -45,17 +45,17 @@ app.kubernetes.io/managed-by: {{ .Release.Service }}
 {{/*
 Selector labels
 */}}
-{{- define "infra-worker.selectorLabels" -}}
-app.kubernetes.io/name: {{ include "infra-worker.name" . }}
+{{- define "api-gateway.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "api-gateway.name" . }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 {{- end }}
 
 {{/*
 Create the name of the service account to use
 */}}
-{{- define "infra-worker.serviceAccountName" -}}
+{{- define "api-gateway.serviceAccountName" -}}
 {{- if .Values.serviceAccount.create }}
-{{- default (include "infra-worker.fullname" .) .Values.serviceAccount.name }}
+{{- default (include "api-gateway.fullname" .) .Values.serviceAccount.name }}
 {{- else }}
 {{- default "default" .Values.serviceAccount.name }}
 {{- end }}
--- athmos/secrets.sh
+++ athmos/secrets.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
+FRONTEND_NAMESPACE=frontend
 COMMON_NAMESPACE=common
 INFRA_NAMESPACE=infra
 GATEWAY_NAMESPACE=gateway
 CERT_MANAGER_NAMESPACE=cert-manager
 
+
+kubectl create namespace "$FRONTEND_NAMESPACE" > /dev/null 2>&1
 kubectl create namespace "$COMMON_NAMESPACE"  > /dev/null 2>&1
 kubectl create namespace "$INFRA_NAMESPACE" > /dev/null 2>&1
 kubectl create namespace "$GATEWAY_NAMESPACE" > /dev/null 2>&1
-kubectL create namespace "$CERT_MANAGER_NAMESPACE" > /dev/null 2>&1
+kubectl create namespace "$CERT_MANAGER_NAMESPACE" > /dev/null 2>&1
+
 
 CLOUDFLARE_SECRET_NAME=cloudflare-secret
 CLOUDFLARE_SECRET_KEY=cloudflare-api-token
-kubectl -n $COMMON_NAMESPACE create secret generic $CLOUDFLARE_SECRET_NAME --from-literal=$CLOUDFLARE_SECRET_KEY=$(cat "$HOME/.athmos/cloudflare") > /dev/null 2>&1
 kubectl -n $CERT_MANAGER_NAMESPACE create secret generic $CLOUDFLARE_SECRET_NAME --from-literal=$CLOUDFLARE_SECRET_KEY=$(cat "$HOME/.athmos/cloudflare") > /dev/null 2>&1
 
-# NEO4J_NAMESPACE=gateway
-# NEO4J_SECRET_NAME=neo4j-secrets
-# NEO4J_PASSWORD=$(openssl rand -base64 15)
+NEO4J_NAMESPACE=gateway
+NEO4J_SECRET_NAME=neo4jsecret
+NEO4J_API_SECRET_NAME=neo4j-gateway-auth
+NEO4J_PASSWORD="$(openssl rand -base64 15)!"
 
-# kubectl create namespace "$NEO4J_NAMESPACE" > /dev/null 2>&1
-# kubectl -n $NEO4J_NAMESPACE create secret generic $NEO4J_SECRET_NAME --from-literal=NEO4J_DATA=$NEO4J_PASSWORD > /dev/null 2>&1
+kubectl create namespace "$NEO4J_NAMESPACE" > /dev/null 2>&1
+kubectl -n $NEO4J_NAMESPACE create secret generic $NEO4J_SECRET_NAME --from-literal=NEO4J_AUTH="neo4j/$NEO4J_PASSWORD" > /dev/null 2>&1
+kubectl -n $NEO4J_NAMESPACE create secret generic $NEO4J_API_SECRET_NAME --from-literal=password=$NEO4J_PASSWORD > /dev/null 2>&1
+
 
 MONGODB_SECRET_NAME=mongodb-auth
 MONGODB_PASSWORD=$(openssl rand -base64 15)
@@ -46,8 +52,23 @@ REGISTRY_SERVER=https://registry.athmos-cloud.com/athmos/
 DOCKER_USERNAME="robot\$kubernetes-puller"
 kubectl -n $INFRA_NAMESPACE create secret docker-registry $REGISTRY_CREDENTIALS_SECRET_NAME \
     --docker-server=$REGISTRY_SERVER --docker-username=$DOCKER_USERNAME --docker-password="$(cat "$HOME/.athmos/registry-pwd")" > /dev/null 2>&1
-
+kubectl -n $GATEWAY_NAMESPACE create secret docker-registry $REGISTRY_CREDENTIALS_SECRET_NAME \
+    --docker-server=$REGISTRY_SERVER --docker-username=$DOCKER_USERNAME --docker-password="$(cat "$HOME/.athmos/registry-pwd")" > /dev/null 2>&1
+kubectl -n $FRONTEND_NAMESPACE create secret docker-registry $REGISTRY_CREDENTIALS_SECRET_NAME \
+    --docker-server=$REGISTRY_SERVER --docker-username=$DOCKER_USERNAME --docker-password="$(cat "$HOME/.athmos/registry-pwd")" > /dev/null 2>&1
 
 OPERATIONS_KUBECONFIG_SECRET_NAME=operations-kubeconfig
 kubectl -n $INFRA_NAMESPACE create secret generic $OPERATIONS_KUBECONFIG_SECRET_NAME \
     --from-file=config="$HOME/.athmos/operation-kubeconfig" > /dev/null 2>&1
+
+
+ADMIN_PASSWORD_SECRET_NAME=admin-secret
+ADMIN_PASSWORD=$(openssl rand -base64 21)
+
+kubectl -n $GATEWAY_NAMESPACE create secret generic $ADMIN_PASSWORD_SECRET_NAME \
+    --from-literal=password=$ADMIN_PASSWORD > /dev/null 2>&1
+
+SECRET_API_SECRET_NAME=secret-api-secret
+SECRET_API_PASSWORD=$(openssl rand -base64 21)
+kubectl -n $GATEWAY_NAMESPACE create secret generic $SECRET_API_SECRET_NAME \
+    --from-literal=secret=$SECRET_API_PASSWORD > /dev/null 2>&1
